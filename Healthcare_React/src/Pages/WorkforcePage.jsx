b1c7c38641c129a38f258b7fa620d9bc578f3c9c
VIREN711 <virenmehta711@gmail.com>
2023-08-27 06:15:07 +0530

    Amazing Viren

--- Healthcare_React/src/Pages/WorkforcePage.jsx
+++ Healthcare_React/src/Pages/WorkforcePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'; 
+import axios from 'axios'; 
 
-const WorkforcePage = () => {
+const Workforce = () => {
+  const [output, setOutput] = useState('');
+
+  const runPythonScript = async () => {
+    try {
+      const data = { notebookPath: "D:\\VIREN\\Health\\healthcare-management-system\\WorkforceAllocation.py" };
+      const response = await axios.post("http://localhost:9999/runwork", data);
+      setOutput(response.data.output);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div>WorkforcePage</div>
-  )
-}
+    <div>
+      <button onClick={runPythonScript}>Optimize Workforce</button>
+      <pre>{output}</pre>
+    </div>
+  );
+};
 
-export default WorkforcePage
+export default Workforce;
--- Healthcare_node/app.js
+++ Healthcare_node/app.js
@@ -9,7 +9,7 @@ const {exec}=require('child_process')
 app.use(cors())
 app.use(express.json());
 app.post("/mail",(req,res)=>{
-  var p={email:"vinitchokshi1809@gmail.com",name:"VINIT"}
+  var p={email:"vinitchokshi1809@gmail.com",name:"Vinit"}
   var dname={brandName:"Dr. Shandy"}
   console.log("sdfgh",req.body)
   email(p,dname,req.body,"res")
@@ -53,7 +53,46 @@ app.post("/run", (req, res) => {
     }
   });
 });
-  
+
+app.post("/runwork", (req, res) => {
+  console.log("1", req); // Check the contents of the request body
+  const notebookPath = req.body.notebookPath;
+  console.log(__dirname);
+  const outputPath = path.join(__dirname, "/WorkforceAllocation.py");
+  console.log(outputPath);
+  exec(`python ${notebookPath}`, (pythonExecutionError, stdout, stderr) => {
+    if (pythonExecutionError) {
+      console.error("Python execution error:", pythonExecutionError);
+      console.log(1);
+      res.status(500).json({ error: "Python script execution failed." });
+      return;
+    } else {
+      const regex = /Status: Optimal/m; // Use the 'm' flag to match multi-line text
+      const match = stdout.match(regex);
+      console.log("Python script output:", stdout);
+      if (match) {
+        const startIndex = match.index;
+        const lines = stdout.split("\n");
+        const indexOfStatusLine = lines.findIndex((line) =>
+          line.includes(match[0])
+        );
+
+        if (indexOfStatusLine !== -1) {
+          const linesBelow = lines.slice(indexOfStatusLine + 1);
+          
+          console.log(linesBelow.join("\n").split('.0'));
+          res.json({ output: linesBelow.join("\n").split('.0') });
+        } else {
+          console.log("Status line found, but lines below not available");
+        }
+      } else {
+        console.log("Status not found");
+      }
+    }
+  });
+});
+
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
